092b8a40411f97b0fd5493d1601ecab1729f2bfd
YiJiang666 <83895050+YiJiang666@users.noreply.github.com>
2022-06-24 18:51:25 +0800

    Add files via upload

---
+++ 2022.6.17测试/编程/T4.c
@@ -0,0 +1,25 @@
+//输入 10 个整数 输出其中最小值及其小标 输出格式:min=arr[3]=26
+#include <stdio.h>
+void fun(int arr[])
+{
+  int min = arr[0], index = 0;
+  for (int i = 1; i < 10; i++)
+  {
+    if (min > arr[i])
+    {
+      min = arr[i];
+      index = i;
+    }
+  }
+  printf("min=arr[%d]=%d", index, min);
+}
+int main(int argc, char const *argv[])
+{
+  int arr[10];
+  for (int i = 0; i < 10; i++)
+  {
+    scanf("%d", &arr[i]);
+  }
+  fun(arr);
+  return 0;
+}
