5ad910436837d81bcc4272fb34f4cd6e772345ea
YiJiang666 <83895050+YiJiang666@users.noreply.github.com>
2022-06-24 18:49:32 +0800

    Add files via upload

---
+++ 2022.6.17测试/编程/T2.c
@@ -0,0 +1,25 @@
+//输出 100 以内的素数
+#include <stdbool.h>
+#include <stdio.h>
+bool fun(int n)
+{
+  for (int i = 2; i <= n / 2; i++)
+  {
+    if (n % i == 0)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+int main(int argc, char const *argv[])
+{
+  for (int i = 2; i < 100; i++)
+  {
+    if (fun(i))
+    {
+      printf("%d\n", i);
+    }
+  }
+  return 0;
+}
