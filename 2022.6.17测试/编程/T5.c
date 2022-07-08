fd4e89d453237f57b592430691e5b1025f2e6ead
YiJiang666 <83895050+YiJiang666@users.noreply.github.com>
2022-07-08 09:22:41 +0800

    Add files via upload

---
+++ 2022.6.17测试/编程/T5.c
@@ -0,0 +1,18 @@
+//输入正整数n 计算并输出1 + 1/2 + 1/3 + 1/4 ··· 1/n 输出结果保留 4 位小数
+#include <stdio.h>
+double fun(int n)
+{
+  double res = 0.0;
+  for (int i = 1; i <= n; i++)
+  {
+    res += 1.0 / i;
+  }
+  return res;
+}
+int main(int argc, char const *argv[])
+{
+  int n;
+  scanf("%d", &n);
+  printf("%.4lf", fun(n));
+  return 0;
+}
